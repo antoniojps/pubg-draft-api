9242fa16013a54fcc82d995ad71898ba05a67ae4
António Santos <antoniojps@ua.pt>
2019-06-21 22:55:20 +0100

    Add production db

--- services/config.example.js
+++ services/config.example.js
@@ -1,5 +1,10 @@
 const FACEIT_API_KEY = 'xxx'
+const PUBG_API_KEY = 'xxx'
+const MONGODB_URI =
+  process.env === 'production' ? 'xxx' : 'mongodb://localhost:27017/PubgDraft'
 
 module.exports = {
-  FACEIT_API_KEY
+  FACEIT_API_KEY,
+  PUBG_API_KEY,
+  MONGODB_URI
 }
