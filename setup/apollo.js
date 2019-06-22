3b554ffb441bf2f0632eca0526b34a2211284c9b
António Santos <antoniojps@ua.pt>
2019-06-22 01:10:04 +0100

    Fix cors

--- setup/apollo.js
+++ setup/apollo.js
@@ -10,6 +10,14 @@ const apolloServer = new ApolloServer({
   playground: true
 })
 
+// Fixed cors, read issue: https://github.com/apollographql/apollo-server/issues/2473
+
 module.exports = function() {
-  return cors(apolloServer.createHandler())
+  return cors((req, res) => {
+    if (req.method === 'OPTIONS') {
+      res.end()
+      return
+    }
+    return apolloServer.createHandler()(req, res)
+  })
 }
